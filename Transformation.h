3299b1eb2996e1b4f9aa0b22579743a9657d0b5d
radzikkos <56590894+radzikkos@users.noreply.github.com>
2020-05-31 18:08:15 +0200

    Add files via upload

---
+++ Transformation.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <array>
+#include <cmath>
+
+#ifndef M_PI
+	#define M_PI 3.14159265358979323846
+#endif
+
+//////////////////////////////////////////////////////////////////////////////////////
+class Vector
+{
+	public:
+		
+		Vector(double x = 0.0f, double y = 0.0f, double z = 0.0f);
+
+		void Set(int i,  double val);
+
+		double Get(int i) const;
+
+	private:
+
+		std::array<double, 4> data;
+};
+
+//////////////////////////////////////////////////////////////////////////////////////
+class Matrix
+{
+	public:
+
+		Matrix();
+
+		void Set(int i, int j, double val);
+
+		double Get(int i, int j) const;
+
+		Matrix operator*(const Matrix &mat) const;
+
+	private:
+
+		std::array<std::array<double, 4>, 4> data;
+};
+
+//////////////////////////////////////////////////////////////////////////////////////
+Vector operator*(const Matrix& mat, const Vector& vec);
+
+Matrix Rotate(double rotate_x, double rotate_y, double rotate_z);
+
+Matrix Perspective(double width, double height, double depth);
